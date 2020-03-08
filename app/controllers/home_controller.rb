ad6c98b7032b7eb69f3f5785a4c53cd09a35911c
Michael Barton <michael.barton@amount.com>
2020-03-08 12:12:24 -0500

    Get hacker news links to show on the page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,17 @@ require 'twitter/client'
 require 'hackernews/client'
 
 class HomeController < ActionController::Base
+  def index
+    @links = load_hackernews_links
+  end
+
   def load_tweets
-    client = PersonalCrm::Twitter::Client.intialize_from_config
+    client = Twitter::Client.initialize_from_config
     client.load_tweets(number: 5)
   end
 
   def load_hackernews_links
-    client = PersonalCrm::HackerNews::Client.initialize_from_config
-    client.load_links(number: 5)
+    client = HackerNews::Client
+    client.links(number: 5)
   end
 end
--- lib/hackernews/client.rb
+++ lib/hackernews/client.rb
@@ -1,11 +1,22 @@
 require 'hackernews_ruby'
+require 'hackernews/link'
 
 module HackerNews
   class Client
 
-    def get_links(number:)
-      HackernewsRuby::Client.new.top.fist(number)
+    def self.links(number:)
+      client.top_stories.first(number).map{ |id| individual_story_details(id) }
     end
+
+    def self.individual_story_details(story_id)
+      HackerNews::Link.new(client.get_item(story_id))
+    end
+    private_class_method :individual_story_details
+
+    def self.client
+      @client ||= HackernewsRuby::Client.new
+    end
+    private_class_method :client
 
   end
 end
