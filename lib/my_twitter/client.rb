0ef3bf25b031a8dc16cdad254c30edfdc050217a
mpbart <michael.barton0924@gmail.com>
2020-03-22 18:29:48 -0500

    Hook up twitter

--- lib/my_twitter/client.rb
+++ lib/my_twitter/client.rb
@@ -1,18 +1,18 @@
+require 'twitter'
 require 'config_reader'
 require 'constants'
-require 'twitter'
 
-module Twitter
+module MyTwitter
   class Client
     
     attr_reader :client
 
     def initialize(config_reader)
       @client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = config_reader.consumer_key
-        config.consumer_secret     = config_reader.consumer_secret
-        config.access_token        = config_reader.access_token
-        config.access_token_secret = config_reader.access_token_secret
+        config.consumer_key        = config_reader['consumer_key']
+        config.consumer_secret     = config_reader['consumer_secret']
+        config.access_token        = config_reader['access_token']
+        config.access_token_secret = config_reader['access_token_secret']
       end
     end
 
