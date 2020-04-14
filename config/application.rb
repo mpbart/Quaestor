d7695746dd27f9f2adef20292b7fd31571b70630
mpbart <michael.barton0924@gmail.com>
2020-04-13 20:52:30 -0500

    Add pry gem

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require_relative 'boot'
 require 'rails/all'
 require 'devise'
 require 'awesome_print'
+require 'pry'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
