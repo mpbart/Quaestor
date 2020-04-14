80a23f1a11b73422ca8bd1f161c8d43daa3bd861
mpbart <michael.barton0924@gmail.com>
2020-04-13 20:58:13 -0500

    Start looking into logic for plaid controller

--- app/controllers/plaid_controller.rb
+++ app/controllers/plaid_controller.rb
@@ -1,6 +1,29 @@
+require 'plaid'
+require 'config_reader'
+
 class PlaidController < ActionController::Base
-  def get_access_token(params)
-    p params
-    PlaidCredentials.store!(params)
+  skip_before_action :verify_authenticity_token
+  TOKEN_REGEX = /\w+-\w+-\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/
+
+  def get_access_token
+    public_token = params['public_token']
+    return unless valid_public_token?(public_token)
+
+    response = plaid_client.item.public_token.exchange(public_token)
   end
+
+  private
+
+  def plaid_client
+    config = ConfigReader.for('plaid')
+    @plaid_client ||= Plaid::Client.new(env:        :sandbox,
+                                        client_id:  config['client_id'],
+                                        secret:     config['secret'],
+                                        public_key: config['public_key'])
+  end
+
+  def valid_public_token?(token)
+    TOKEN_REGEX =~ token
+  end
+
 end
