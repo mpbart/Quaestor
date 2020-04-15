b27bad3148f087daaf8d8386c2d1020fef14ec6d
mpbart <michael.barton0924@gmail.com>
2020-04-14 21:17:51 -0500

    Store plaid credentials when creating a new access token

--- app/controllers/plaid_controller.rb
+++ app/controllers/plaid_controller.rb
@@ -2,6 +2,7 @@ require 'plaid'
 require 'config_reader'
 
 class PlaidController < ActionController::Base
+  before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
   TOKEN_REGEX = /\w+-\w+-\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/
 
@@ -10,6 +11,9 @@ class PlaidController < ActionController::Base
     return unless valid_public_token?(public_token)
 
     response = plaid_client.item.public_token.exchange(public_token)
+    PlaidCredential.create!(plaid_item_id: response['item_id'],
+                            access_token: response['access_token'],
+                            user: current_user)
   end
 
   private
