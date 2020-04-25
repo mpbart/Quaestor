b794a92da46d0fd1c4aa70a29bb1a6a5c5e3a6d7
mpbart <michael.barton0924@gmail.com>
2020-04-25 12:26:25 -0500

    Create logic for handling plaid operations outside of controllers and models

--- app/models/plaid_credential.rb
+++ app/models/plaid_credential.rb
@@ -2,5 +2,7 @@ require 'attr_encrypted'
 
 class PlaidCredential < ActiveRecord::Base
   belongs_to :user
+  has_many :plaid_responses
+
   attr_encrypted :access_token, key: ENV['ENCRYPTION_KEY']
 end
--- app/models/plaid_response.rb
+++ app/models/plaid_response.rb
@@ -4,4 +4,12 @@ class PlaidResponse < ApplicationRecord
   belongs_to :plaid_credential
 
   attr_encrypted :response, key: ENV['ENCRYPTION_KEY'], marshal: true
+
+  def self.record_accounts_response!(response, credential)
+    PlaidResponse.create!(
+      endpoint:         '/accounts/get',
+      response:         response,
+      plaid_credential: credential,
+    )
+  end
 end
