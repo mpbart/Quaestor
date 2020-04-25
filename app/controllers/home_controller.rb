4de508c222fd34f5cc2762c0e2dfd9b1eb9f9bf4
mpbart <michael.barton0924@gmail.com>
2020-04-25 11:18:38 -0500

    Remove unused social media setup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,9 @@
-require 'my_twitter/client'
-require 'hackernews/client'
-
 class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @links = load_hackernews_links
-    @tweets = load_tweets
+    @links ||= []
+    @tweets ||= []
   end
 
-  def load_tweets
-    client = MyTwitter::Client.initialize_from_config
-    client.get_tweets(number: 5)
-  end
-
-  def load_hackernews_links
-    client = HackerNews::Client
-    client.links(number: 5)
-  end
 end
