8b93b20ec96957b082dff61e9372c1b7c64d119f
mpbart <michael.barton0924@gmail.com>
2020-05-31 11:20:10 -0500

    Add scope for sorting transactions by date

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,7 +2,7 @@ class TransactionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @transactions = current_user.paginated_transactions(page_num: 1)
+    @transactions = current_user.paginated_transactions(page_num: 1).by_date
   end
 
 end
