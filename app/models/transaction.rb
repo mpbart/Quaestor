19cfb1c2e0eeee776d0a95412074701d1d591ced
mpbart <michael.barton0924@gmail.com>
2020-07-12 11:31:02 -0500

    Add callback to update category properly on transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,13 @@
 class Transaction < ApplicationRecord
   belongs_to :account
   belongs_to :user
+  before_update :normalize_category
 
   scope :by_date, -> { order('date DESC') }
+
+  private
+
+  def normalize_category
+    self.category = PlaidCategory.find_by(category_id: category_id).hierarchy
+  end
 end
