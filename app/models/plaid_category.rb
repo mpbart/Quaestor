36375e5705808b275e74c019c178d6571addb815
mpbart <michael.barton0924@gmail.com>
2020-08-09 12:26:19 -0500

    Add methods to PlaidCategory model to support grouping more easily

--- app/models/plaid_category.rb
+++ app/models/plaid_category.rb
@@ -5,6 +5,18 @@ class PlaidCategory < ActiveRecord::Base
     @@top_level ||= PlaidCategory.all.group_by{ |category| category.hierarchy[0] }
   end
 
+  def self.top_level_records
+    @@top_level_records ||= where("jsonb_array_length(hierarchy) = 1").to_a
+  end
+
+  def self.category_children(top_level_key)
+    grouped_by_top_level.dig(top_level_key)
+  end
+
+  def children
+    PlaidCategory.grouped_by_top_level.dig(hierarchy.first)
+  end
+
   # Get all categories that match the given key
   # Note: that this assumes that keys are passed in hierarchical order
   def self.all_matching_categories(key:)
