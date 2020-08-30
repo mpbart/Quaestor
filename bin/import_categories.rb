413c2c1cf0a22ccda14ff37053c266e1ac22d86a
mpbart <michael.barton0924@gmail.com>
2020-08-30 14:02:13 -0400

    Create script to re-initialize plaid categories

---
+++ bin/import_categories.rb
@@ -0,0 +1,11 @@
+require 'plaid'
+require 'config_reader'
+
+# Use this script to re-initialize the plaid_categories database table if it
+# gets wiped
+
+config = ConfigReader.for('plaid')
+client = Plaid::Client.new(env: :sandbox, client_id: config['client_id'], secret: config['secret'])
+client.categories.get['categories'].each do |cat|
+  PlaidCategory.create!(hierarchy: cat.hierarchy, category_id: cat.category_id)
+end
