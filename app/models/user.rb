830e892137dd2bcba8c3dc2d3fc6ca65afeeb4ce
mpbart <michael.barton0924@gmail.com>
2020-11-17 21:59:40 -0500

    Show both normal and split transactions in the transactions page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,14 @@ class User < ApplicationRecord
 
   has_many :accounts
   has_many :transactions, through: :accounts
+  has_many :split_transactions, through: :transactions
   has_many :plaid_credentials
 
   # Only show 50 transactions at a time
   def paginated_transactions(page_num:)
-    transactions.paginate(page: page_num, per_page: 50)
+    t = transactions.by_date.paginate(page: page_num, per_page: 45)
+    s = split_transactions.by_date.where('split_transactions.date BETWEEN ? AND ?', t.last.date, t.first.date)
+    (t + s).sort_by{ |i| i.date }
   end
 
 end
