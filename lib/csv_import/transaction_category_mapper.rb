c1e0937777f83dcbc80dbd80b11c6cb3c6b34f74
mpbart <michael.barton0924@gmail.com>
2021-08-07 12:07:10 -0400

    Add mappings for all mint categories --> plaid categories

--- lib/csv_import/transaction_category_mapper.rb
+++ lib/csv_import/transaction_category_mapper.rb
@@ -8,8 +8,8 @@ module CsvImport
       @source_mappings = load_source_mappings(source.to_s)
     end
 
-    def [](key)
-      category = @source_mappings.dig(key)
+    def [](transaction)
+      category = @source_mappings.map_category(transaction.dig('Category'), transaction.dig('Description'))
 
       raise MappingNotFoundError.new("Could not find mapping for category #{key}") unless category
 
@@ -21,7 +21,7 @@ module CsvImport
     def load_source_mappings(source)
       require 'csv_import/constants/' + source
       klass = "CsvImport::Constants::#{source.camelize}".constantize
-      klass.mappings
+      klass.new
     end
 
   end
