e925436fce8b59c1d7e078e907c1cd201d001b87
mpbart <michael.barton0924@gmail.com>
2024-01-20 14:13:29 -0500

    Fix bug in config_reader

--- lib/config_reader.rb
+++ lib/config_reader.rb
@@ -4,12 +4,12 @@ require 'json'
 
 class ConfigReader
   def self.for(client_name)
-    config_file.dig[client_name]
+    config_file[client_name]
   end
 
   def self.config_file
     File.open(Rails.root.join('config.json'), 'r') do |file|
-      JSON.load(file)
+      JSON.parse(file.read)
     end
   end
 end
