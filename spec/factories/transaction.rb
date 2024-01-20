1e449c09ef657f42ea3ab38443e653f50479ec4d
mpbart <michael.barton0924@gmail.com>
2024-01-20 12:55:24 -0500

    Fill out transaction factory

--- spec/factories/transaction.rb
+++ spec/factories/transaction.rb
@@ -8,6 +8,12 @@ FactoryBot.define do
     description { "test transaction" }
     amount { 10.0 }
     date { Date.current }
+    category_confidence { 'HIGH' }
+    merchant_name       { 'merchant' }
+    payment_channel     { 'in store' }
+    payment_metadata    { {} }
+    location_metadata   { {} }
     pending { false }
+    account_owner       { account.mask }
   end
 end
