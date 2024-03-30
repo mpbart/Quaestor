d3d2f76da9c48f8b1877f85298cc6aaade6e3e5f
mpbart <michael.barton0924@gmail.com>
2024-03-30 11:30:51 -0400

    Fully implement create balance form

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -3,7 +3,15 @@
 class BalancesController < ApplicationController
   def create
     account = ::Account.find(params[:balance][:account_id])
-    FinanceManager::Account.create_balance(account, params[:balance])
+    # TODO: Refactor this when adding a repository/persistence layer
+    # Ignore available balance and limit when manually creating balances
+    balance = FinanceManager::Account::BalanceStruct.new(
+      BigDecimal(params[:balance][:balance]),
+      0,
+      0
+    )
+    FinanceManager::Account.create_balance(account, balance)
+    render json: { success: true }
   rescue StandardError => e
     render json: { success: false, error: e }
   end
