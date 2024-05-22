04c51518416b68db49edc87ee42f9c7e7339f002
Mike Barton <michael.barton0924@gmail.com>
2024-05-21 22:30:13 -0400

    Use account type to calculate net worth instead of summing all account balances (#121)

--- app/models/account.rb
+++ app/models/account.rb
@@ -19,4 +19,27 @@ class Account < ActiveRecord::Base
                      'profit_sharing_plan', 'roth', 'roth 401k', 'trust', 'ugma', 'utma'],
     'other'      => []
   }.freeze
+
+  NET_WORTH_SQL = <<-SQL
+    WITH debts AS (
+      SELECT SUM(balances.amount) FROM accounts
+      JOIN (SELECT *, ROW_NUMBER() OVER (PARTITION BY account_id ORDER BY created_at DESC) FROM balances)
+      AS balances ON balances.account_id = accounts.id
+      WHERE row_number = 1 AND accounts.account_type IN ('loan', 'credit')
+      AND accounts.user_id = ?
+      ),
+    assets AS (
+      SELECT SUM(balances.amount) FROM accounts
+      JOIN (SELECT *, ROW_NUMBER() OVER (PARTITION BY account_id ORDER BY created_at DESC) FROM balances)
+      AS balances ON balances.account_id = accounts.id
+      WHERE row_number = 1 AND accounts.account_type IN ('depository', 'investment')
+      AND accounts.user_id = ?
+    )
+    SELECT assets.sum - debts.sum AS net_worth FROM assets, debts
+  SQL
+
+  def self.net_worth(user_id)
+    sanitized_sql = ActiveRecord::Base.send(:sanitize_sql_array, [NET_WORTH_SQL, user_id, user_id])
+    ActiveRecord::Base.connection.execute(sanitized_sql, user_id).first['net_worth']
+  end
 end
