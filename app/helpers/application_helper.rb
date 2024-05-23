9dd7b7096bdcd5bd091def94f420a19687e23a0f
mpbart <michael.barton0924@gmail.com>
2024-05-23 09:19:02 -0400

    Start redesigning the home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def inline_svg(path)
+    file_path = Rails.root.join('app', 'assets', 'images', path)
+    File.exist?(file_path) ? File.read(file_path).html_safe : 'no svg found'
+  end
 end
