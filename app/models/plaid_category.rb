2bd870380a30e27342d712baeff533b67f67b194
Mike Barton <michael.barton0924@gmail.com>
2024-07-13 15:19:33 -0400

    Remove unneeded code

--- app/models/plaid_category.rb
+++ app/models/plaid_category.rb
@@ -14,30 +14,4 @@ class PlaidCategory < ActiveRecord::Base
                             RENT_AND_UTILITIES_TELEPHONE
                             RENT_AND_UTILITIES_WATER INCOME_WAGES
                             INCOME_DIVIDENDS INCOME_INTEREST_EARNED].freeze
-
-  def self.grouped_by_top_level
-    @grouped_by_top_level ||= PlaidCategory.all.group_by(&:primary_category)
-  end
-
-  def self.top_level_records
-    @top_level_records ||= select(:primary_category).distinct.to_a
-  end
-
-  def self.category_children(top_level_key)
-    grouped_by_top_level[top_level_key]
-  end
-
-  def children
-    PlaidCategory.grouped_by_top_level[primary_category]
-  end
-
-  # Get all categories that match the given key
-  # Note: that this assumes that keys are passed in hierarchical order
-  def self.all_matching_categories(key:)
-    categories = grouped_by_top_level[key.shift]
-
-    return categories unless key
-
-    categories.select { |c| c.hierarchy.include?(key.last) }
-  end
 end
