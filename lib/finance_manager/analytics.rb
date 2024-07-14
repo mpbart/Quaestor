dd922794590e451b98740c78388ea6c5b9529afa
Mike Barton <michael.barton0924@gmail.com>
2024-07-14 10:02:35 -0400

    Add more SQL for analytics queries

--- lib/finance_manager/analytics.rb
+++ lib/finance_manager/analytics.rb
@@ -23,33 +23,45 @@ module FinanceManager
       end
     end
 
-    # Return JSON array of amounts spent on the category_name within
-    # the passed in timeframe in months
+    # Return JSON array of amounts spent by month on the category_name
+    # within the passed in timeframe
     # TODO: currently hardcoded to 1 year
-    def self.spending_on_category_over_timeframe(category_name, timeframe)
+    def self.spending_on_primary_category_over_timeframe(category_name, timeframe, user_id)
+      Transaction.primary_category_spending_over_time(category_name, user_id)
     end
 
-    # Return JSON array of amounts spent on the merchant_name within
-    # the passed in timeframe in months
+    # Return JSON array of amounts spent by month on the category_name
+    # within the passed in timeframe
     # TODO: currently hardcoded to 1 year
-    def self.spending_on_merchant_over_timeframe(merchant_name, timeframe)
+    def self.spending_on_detailed_category_over_timeframe(category_name, timeframe, user_id)
+      Transaction.detailed_category_spending_over_time(category_name, user_id)
     end
 
-    # Return JSON array tallying the amounts spent on each category
-    # within the given timeframe in months
+    # Return JSON array of amounts spent by month on the merchant_name within
+    # the passed in timeframe
     # TODO: currently hardcoded to 1 year
-    def self.total_spending_on_all_categories_over_timeframe(timeframe)
+    def self.spending_on_merchant_over_timeframe(merchant_name, timeframe, user_id)
+      Transaction.merchant_spending_over_time(merchant_name, user_id)
+    end
+
+    # Return JSON array tallying the total amounts spent on each category
+    # within the given timeframe
+    # TODO: currently hardcoded to 1 year
+    def self.total_spending_on_all_categories_over_timeframe(timeframe, user_id)
+      Transaction.category_totals(user_id)
     end
 
     # Return JSON array of spending amounts over the given timeframe
     # in months
     # TODO: currently hardcoded to 1 year
-    def self.spending_over_timeframe(timeframe)
+    def self.spending_over_timeframe(timeframe, user_id)
+      Transaction.total_spending_over_time(user_id)
     end
 
     # Return JSON array of income amounts over the given timeframe in months
     # TODO: currently hardcoded to 1 year
-    def self.income_over_timeframe(timeframe)
+    def self.income_over_timeframe(timeframe, user_id)
+      Transaction.total_income_over_time(user_id)
     end
   end
 end
