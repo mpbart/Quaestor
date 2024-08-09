58d5ec92b9efa2dbb3ba25917b7e00912aba522c
mpbart <michael.barton0924@gmail.com>
2024-08-08 22:07:42 -0400

    Intermediate state of adding a form to control the chart

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -25,30 +25,88 @@ const createChart = (data) => ({
         },
         {
           type: 'bar',
-          label: 'Assets',
-          data: data.map(row => row.assets),
-          backgroundColor: '#179d89'
-        },
-        {
-          type: 'bar',
           label: 'Debts',
           data: data.map(row => row.debts),
           backgroundColor: '#9d172b'
+        },
+        {
+          type: 'bar',
+          label: 'Assets',
+          data: data.map(row => row.assets),
+          backgroundColor: '#179d89'
         }
+      ]
+    },
+    options: {
+      scales: { x: { stacked: true }, y: { stacked: true } }
+    }
+  },
+  'spending_on_merchant_over_timeframe': {
+    type: 'bar',
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: 'Merchant Spending over Time',
+          font: {
+            size: 16
+          }
+        }
+      }
+    },
+    data: {
+      labels: data.map(row => row.month),
+      datasets: [
+        {
+          type: 'bar',
+          label: 'Spending',
+          data: data.map(row => row.debts),
+          backgroundColor: '#9d172b'
+        },
       ]
     }
   }
 });
 
+let analyticsChart = null;
+
 
 $(function() {
+  $('#reportType').change(function() {
+    var selectedOption = $(this).val();
+    $('#merchantField').hide();
+    $('#categoryField').hide();
+
+    if (selectedOption === 'spending_on_merchant_over_timeframe') {
+      $('#merchantField').show();
+    } else if (selectedOption === 'spending_by_category') {
+      $('#categoryField').show();
+    }
+  });
+
+  $('#chartForm').submit(function(event) {
+    event.preventDefault();
+    renderChart();
+  });
+
+  renderChart();
+});
+
+renderChart = function() {
+  const chart_type = $('#reportType').val() || 'net_worth_over_timeframe';
   getData = async function() {
-    const response = await fetch('/chart_data/net_worth_over_timeframe', { method: 'GET' });
+    // TODO: Send the parameters from the form as part of the GET call to the
+    // backend
+    const response = await fetch(`/chart_data/${chart_type}`, { method: 'GET' });
     return await response.json();
   }
 
   getData().then(data => {
-    const chart = createChart(data)['net_worth_over_timeframe'];
-    new Chart($('#analytics'), chart);
+    if (analyticsChart !== null) {
+      analyticsChart.destroy();
+    }
+
+    const chart = createChart(data)[chart_type];
+    analyticsChart = new Chart($('#analytics'), chart);
   });
-});
+}
--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -9,7 +9,13 @@ class AnalyticsController < ApplicationController
 
   def chart_data
     data_set = params.permit(:dataset)[:dataset]
-    results = FinanceManager::Analytics.try(data_set, nil, current_user.id)
+    # TODO: make all parameters keyword args so that we can dynamically pass in
+    # different numbers of args to the analytics methods
+    results = if ['spending_on_merchant_over_timeframe'].include?(data_set)
+                FinanceManager::Analytics.try(data_set, 'meijer', nil, current_user.id)
+              else
+                FinanceManager::Analytics.try(data_set, nil, current_user.id)
+              end
     render json: results || {}
   end
 end
