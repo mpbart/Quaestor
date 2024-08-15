c0dcbd2343eda3d6e25d8c4a0690ac4e190c0136
Mike Barton <michael.barton0924@gmail.com>
2024-08-15 08:44:39 -0400

    Smooth out rough edges when calling into analytics methods from controller

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -9,17 +9,13 @@ class AnalyticsController < ApplicationController
 
   def chart_data
     data_set = params.permit(:dataset, :timeframe, :category, :merchant_name)
-    # TODO: make all parameters keyword args so that we can dynamically pass in
-    # different numbers of args to the analytics methods
-    results = if ['spending_on_merchant_over_timeframe'].include?(data_set[:dataset])
-                FinanceManager::Analytics.try(data_set[:dataset], data_set[:merchant_name], nil,
-                                              current_user.id)
-              elsif ['spending_on_category_over_timeframe'].include?(data_set[:dataset])
-                FinanceManager::Analytics.try(:spending_on_detailed_category_over_timeframe,
-                                              data_set[:category], nil, current_user.id)
-              else
-                FinanceManager::Analytics.try(data_set[:dataset], nil, current_user.id)
-              end
+    params = {
+      category_name: data_set[:category],
+      merchant_name: data_set[:merchant_name],
+      user_id:       current_user.id,
+      _timeframe:    data_set[:timeframe]
+    }.compact
+    results = FinanceManager::Analytics.try(data_set[:dataset], **params)
     render json: results || {}
   end
 end
