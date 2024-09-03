f5046ee0d086a289acd5423ac31adb794b98d001
mpbart <michael.barton0924@gmail.com>
2024-09-02 21:00:59 -0400

    Remov hypopg from the db extensions

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_08_25_131629) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "hypopg"
   enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
