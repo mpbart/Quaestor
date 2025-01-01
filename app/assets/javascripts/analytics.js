c9bdd84f09787bc2bb251f39a0034b16bc1b90f9
mpbart <michael.barton0924@gmail.com>
2025-01-01 09:34:54 -0500

    More analytics page fixes

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -107,19 +107,29 @@ renderChart = function(form, chartCreator) {
 currentDate = function() {
   const date = new Date(Date.now());
   let day = date.getDate();
-  const month = date.getMonth();
+  let month = date.getMonth();
+  if (month < 10) {
+    month = `0${month + 1}`; 
+  } else {
+    month = month + 1;
+  }
   const year = date.getFullYear();
   if (day < 10) {
     day = `0${day}`
   }
-  return `${year}-${month + 1}-${day}`;
+  return `${year}-${month}-${day}`;
 }
 
 oneYearAgoMonthlyDate = function() {
   const date = new Date(Date.now());
-  const month = date.getMonth();
+  let month = date.getMonth();
+  if (month < 10) {
+    month = `0${month + 1}`; 
+  } else {
+    month = month + 1;
+  }
   const year = date.getFullYear();
-  return `${year - 1}-${month + 1}-01`;
+  return `${year - 1}-${month}-01`;
 }
 
 beginningOfCurrentYearDate = function() {
