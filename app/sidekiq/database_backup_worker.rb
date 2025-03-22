771826ad64e1b25fa316161364a72abf72eef14c
mpbart <michael.barton0924@gmail.com>
2025-03-22 16:41:32 -0400

    Implement database backup uploads to google drive

--- app/sidekiq/database_backup_worker.rb
+++ app/sidekiq/database_backup_worker.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
+require 'finance_manager/google_api_client'
+
 class DatabaseBackupWorker
   include Sidekiq::Worker
 
   def perform
+    create_local_backup
+    ::FinanceManager::GoogleApiClient.upload_latest_backup_to_drive
+    ::FinanceManager::GoogleApiClient.cleanup_old_backups
+  end
+
+  def create_local_backup
     `#{Rails.root}/bin/backup_db.sh crm_dev`
   end
 end
