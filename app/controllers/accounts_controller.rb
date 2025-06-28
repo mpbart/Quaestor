0870c7ee13144c91fd167d28371314db54d15725
mpbart <michael.barton0924@gmail.com>
2025-06-28 09:54:37 -0400

    add chart on individual account page showing balances

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'finance_manager/analytics'
+
 class AccountsController < ApplicationController
   def create
     FinanceManager::Account.create_manually(params[:account], current_user)
@@ -10,6 +12,18 @@ class AccountsController < ApplicationController
 
   def show
     @account = Account.find(params[:id])
+  end
+
+  def balance_history
+    end_date = Date.current
+    start_date = end_date - 11.months
+    balance_data = FinanceManager::Analytics.account_balance_history(
+      user_id:    current_user.id,
+      account_id: params[:id],
+      start_date: start_date.to_s,
+      end_date:   end_date.to_s
+    )
+    render json: balance_data
   end
 
   def subtypes
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { Turbo } from "@hotwired/turbo-rails"
+import "account_balance_chart"
