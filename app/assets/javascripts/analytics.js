33e13785690d9cb8c4fc8700ecf886fb48610394
mpbart <michael.barton0924@gmail.com>
2025-12-31 11:30:13 -0500

    Read secrets from .env file

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,5 +1,5 @@
 var analyticsChart = null;
-const chartColors = [
+var chartColors = [
   '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
   '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',
   '#aec7e8', '#ffbb78', '#98df8a', '#ff9896', '#c5b0d5',
